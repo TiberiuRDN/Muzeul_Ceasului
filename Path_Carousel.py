2bc089fa277f0da90a5007d3d24988f9a9235785
TiberiuRDN <140619750+TiberiuRDN@users.noreply.github.com>
2024-08-28 20:50:38 +0300

    Add files via upload

---
+++ Path_Carousel.py
@@ -0,0 +1,35 @@
+
+# Import module
+import os
+import pathlib
+__package__ = None
+
+# # Assign directory
+# directory = "/Users/tiberiuradan/PycharmProjects/DataAnalysis/Muzeul_Ceasului/Photo_Gallery"
+# path_list = []
+# image_path = "image_path"
+# gallery_path = "/Users/tiberiuradan/PycharmProjects/DataAnalysis/Muzeul_Ceasului/Photo_Gallery"
+#
+# #Iterate over files in directory
+# for name in os.listdir(directory):
+#     path_list.append(f"{gallery_path}/{name}")
+# with open("/Users/tiberiuradan/PycharmProjects/DataAnalysis/Muzeul_Ceasului/Final_Dictionary.py", "a")  as file:
+#     file.write(f"\n{image_path} = {str(path_list)}")
+
+
+
+
+# Carousel function
+# Remember that each function needs to have a return statement at the end of it in order to output the intended result
+def carousel_function(title, text, img, link):
+    test_items = dict( title= title ,
+          text = text,
+          img= img,
+          link = link )
+    return test_items
+
+
+
+
+
+
---
+++ Web_app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from streamlit_carousel import carousel
+from Final_Dictionary import final_dictionary, image_path
+import Path_Carousel
+
+__package__ = None
+import sys
+print(sys.path)
+
+
+# Convert the list of dictionaries to a language-keyed dictionary
+backbone_dict = {entry['language']: entry for entry in final_dictionary}
+
+# 'RO' as the default language and initiating the st.session_state
+if 'selected_language' not in st.session_state:
+    st.session_state['selected_language'] = 'RO'
+
+# Sidebar for language selection
+with st.sidebar:
+    for lang_code in backbone_dict.keys():
+        if st.button(backbone_dict[lang_code]['language'], key=lang_code):
+            st.session_state['selected_language'] = lang_code
+
+
+# Access the selected entry directly
+selected_entry = backbone_dict[st.session_state['selected_language']]
+
+# Image Carousel
+image_list = []
+for x in image_path:
+    image_list.append(Path_Carousel.carousel_function(title="", text="", img=str(x), link=""))
+
+# Display the content
+carousel(items=image_list, width=1.5)
+st.title(selected_entry['title'])
+st.write(selected_entry['intro'])
+if selected_entry['audio'] != 'None':
+    st.audio(selected_entry['audio'])
+
+
+
+
+
+# # QR code
+# qr = qrcode.make('http://192.168.0.34:8501/')
+# qr.save('/Users/tiberiuradan/Desktop/QR/Demo.png')
+# Remember, when deploying from github, packages needs to be stored in a folder named "requirements.txt" while for
+# module import you need to store these into a "readme.txt" file
